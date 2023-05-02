caebb2654130c96f52e9c63b7c50b047962fe31d
lilbutterflly <tsara6317@gmail.com>
2023-05-02 09:26:48 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _puts - Prints a string
+  * @str: String
+  * Return: Void
+  */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}

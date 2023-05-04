5b0840d3d6a20d1809bc45dc9b250f9e803591cd
lilbutterflly <tsara6317@gmail.com>
2023-05-04 05:25:43 -0700

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * string_toupper - Convert letters to uppercase
+  * @str: String
+  * Return: Returns string
+  */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}

113c09be7200ad49e9fb8ebbae8254e49600f845
lilbutterflly <tsara6317@gmail.com>
2023-05-24 10:25:59 -0700

    operation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  * op_add - returns sum of a and b
+  * @a: int
+  * @b: int
+  * Return: int
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - substruction
+  * @a: int
+  * @b: int
+  * Return: int
+  */
+int op_sub(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_mul - multiplication
+  * @a: int
+  * @b: int
+  * Return: int
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - division
+  * @a: int
+  * @b: int
+  * Return: int
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - mod
+  * @a: int
+  * @b: int
+  * Return: int
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

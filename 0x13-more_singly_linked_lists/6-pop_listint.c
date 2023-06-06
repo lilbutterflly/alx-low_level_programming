7967554eefbbac7b34c3a8303bd1117fdf8f3bb0
lilbutterflly <tsara6317@gmail.com>
2023-06-06 13:23:00 -0700

    delete head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,13 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *ptr;
+	int num;
 
-	if (*head == NULL)
+	if (!head || !*head)
 		return (0);
+	num = (*head)->n;
 	ptr = (*head)->next;
 	free(*head);
 	(*head) = ptr;
-	return ((*head)->n);
+	return (num);
 }
